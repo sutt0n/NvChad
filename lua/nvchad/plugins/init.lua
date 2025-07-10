510c27cff026025fe9bea90e0320c1f649807b5c
Jojo Sutton <sutt0n@users.noreply.github.com>
2025-07-10 15:01:07 -0500

    Update init.lua

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -137,7 +137,7 @@ return {
         "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
-        "https://codeberg.org/FelipeLema/cmp-async-path.git"
+        "https://codeberg.org/sutt0n/cmp-async-path.git"
       }
     },
     opts = function()
